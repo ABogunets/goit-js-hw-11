b8b81a945d0c67781dd3ff907b89ce2ffb378753
ABogunets <abogunets@gmail.com>
2023-02-03 23:56:21 +0200

    working1

--- src/index.js
+++ src/index.js
@@ -1,54 +1,96 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-import countryInfoTpl from './templates/country-info.hbs';
-import countryListTpl from './templates/country-list.hbs';
-import API from "./fetchCountries.js";
+import picsTpl from './templates/pics-list.hbs';
+import API from "./fetchPics.js";
 
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
 
-const inputRef = document.getElementById("search-box");
-const countryInfoRef = document.querySelector(".country-info");
-const countryListRef = document.querySelector(".country-list");
+const formRef = document.querySelector(".search-form");
+const picsContainerRef = document.querySelector(".gallery");
+// const countryListRef = document.querySelector(".country-list");
 
 
-inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+formRef.addEventListener('submit', onSearch);
 
-function onInput(e) {
-  clearCountryList();
-  const countryName = e.target.value.trim();
-
-  if (countryName === '') {
-    return;
-  }
-  API.fetchCountries(countryName)
-    .then(countryCards => {
-    const numberOfCountries = countryCards.length;
-
-    if (numberOfCountries > 10) Notify.info("Too many matches found. Please enter a more specific name.");
-    else if (numberOfCountries >= 2 && numberOfCountries <= 10) {
-      renderCountryList(countryCards);
-    }
-    else if (numberOfCountries === 1) {
-      renderCountryInfo(countryCards);
-    }
-    }) 
-  .catch(onFetchError);
+function onSearch(e) {
+  e.preventDefault();
+  
+  clearPicsList();
+  API.searchOptions.q = e.currentTarget.searchQuery.value;
+  console.log('searchQuery :>> ', API.searchOptions.q);
+  API.fetchPics()
+    .then((pics) => {
+      const picsArray = pics.hits;
+      console.log('picsArray :>> ', picsArray);
+      // const picsMarkup = pics.hits.reduce(
+      //   (markup, pic) => createMarkup(pic) + markup,
+      //   ""
+      // );
+      // console.log('picsMarkup :>> ', picsMarkup);
+      renderPicsMarkup(picsArray);
+    })
+    .catch(onFetchError);
 }
-function renderCountryInfo(card) {
-  const markup = countryInfoTpl(card);
-    countryInfoRef.innerHTML = markup;
+// function createMarkup({webformatURL}) {
+//   return `
+// <div class="photo-card">
+//   <img src=${webformatURL} alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div    `;
+// }
+
+
+
+
+// function onInput(e) {
+//   clearCountryList();
+//   const countryName = e.target.value.trim();
+
+//   if (countryName === '') {
+//     return;
+//   }
+//   API.fetchCountries(countryName)
+//     .then(countryCards => {
+//     const numberOfCountries = countryCards.length;
+
+//     if (numberOfCountries > 10) Notify.info("Too many matches found. Please enter a more specific name.");
+//     else if (numberOfCountries >= 2 && numberOfCountries <= 10) {
+//       renderCountryList(countryCards);
+//     }
+//     else if (numberOfCountries === 1) {
+//       renderCountryInfo(countryCards);
+//     }
+//     }) 
+//   .catch(onFetchError);
+// }
+function renderPicsMarkup(pics) {
+  const markup = picsTpl(pics);
+  console.log('markup :>> ', markup);
+    picsContainerRef.insertAdjacentHTML('beforeend', markup);
 }
-function renderCountryList(card) {
-  const markup = countryListTpl(card);
-    countryListRef.innerHTML = markup;
+
+
+function clearPicsList() {
+  picsContainerRef.innerHTML = "";
+
 }
-function clearCountryList() {
-  countryListRef.innerHTML = "";
-  countryInfoRef.innerHTML = "";
-}
+
 function onFetchError(error) {
   console.log('error :>> ', error);
-  Notify.failure("Oops, there is no country with that name")
+  Notify.failure("Sorry, there are no images matching your search query. Please try again.")
 }
