ae5dd3924f7a1d9b0b74ed98029de3c14a753c19
ABogunets <abogunets@gmail.com>
2023-02-10 18:24:24 +0200

    url line corrected

--- src/pics-api.js
+++ src/pics-api.js
@@ -1,19 +1,12 @@
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "33341846-2d80dabb5272f7d922b758ae2";
-// const searchOptions = {
-//   key: "33341846-2d80dabb5272f7d922b758ae2",
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-//   page: 1,
-//   per_page: 4,
-// };
+
 
 export default class PicsApi {
   constructor() {
     this.searchQuery = '';
     this.queryPage = 1;
-    this.queryPerPage = 20;
+    this.queryPerPage = 40;
     this.image_type = 'photo';
     this.orientation = 'horizontal';
     this.safesearch = true;
@@ -21,9 +14,7 @@ export default class PicsApi {
 
   fetchPics() {
 
-    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&page=${this.queryPage}
-    &per_page=${this.queryPerPage}&image_type=${this.image_type}
-    &orientation=${this.orientation}&safesearch=${this.safesearch}`;
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&page=${this.queryPage}&per_page=${this.queryPerPage}&image_type=${this.image_type}&orientation=${this.orientation}&safesearch=${this.safesearch}`;
   
     return fetch(url)
       .then(response => {
